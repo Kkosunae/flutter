52883c436713c074aa515d10bd3f040d104ec711
Harry <77602040+harryjeonn@users.noreply.github.com>
2023-09-11 01:12:35 +0900

    Added splash screen (#4)
    * Feat #98 - Updated pubspec.yaml, added flutter_native_splash
    
    * Feat #98 - Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'package:kkosunae/src/screens/main_screen.dart';
+import 'package:kkosunae/src/screens/splash_screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await NaverMapSdk.instance.initialize(clientId: 'm3lw8o5fjq');
   KakaoSdk.init(nativeAppKey: 'ae6bcf099a4885a217f4cd8a63548c5e');
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const App());
 }
 
@@ -15,8 +17,9 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
-        home: MainScreen()
+        home: SplashScreen()
     );
   }
 }
